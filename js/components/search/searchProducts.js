0410e08a86a07f70c81a45a46c7f1a8ee2e78356
ainabjerke <83291997+ainabjerke@users.noreply.github.com>
2022-09-08 11:48:58 +0200

    update admin btn and seach input

--- js/components/search/searchProducts.js
+++ js/components/search/searchProducts.js
@@ -4,14 +4,15 @@ export function searchProducts(products) {
   const search = document.querySelector(".search__input");
 
   search.onkeyup = function (event) {
-    const searchValue = event.target.value;
-
+    // const searchValue = event.target.value;
+    const searchValue = event.target.value.toLowerCase();
     const filteredProducts = products.filter(function (product) {
       if (
         product.title.toLowerCase().includes(searchValue) ||
         product.description.toLowerCase().includes(searchValue)
       ) {
         return true;
+        // return product.name.toLowerCase().includes(search);
       }
     });
 
