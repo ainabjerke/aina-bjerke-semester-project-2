073ec86ecdfada58799976bd4fdbe6d22c08b546
ainabjerke <83291997+ainabjerke@users.noreply.github.com>
2022-09-13 19:09:56 +0200

    rearrange scss files

--- js/components/html/createProduct.js
+++ js/components/html/createProduct.js
@@ -17,14 +17,13 @@ export function createProduct(products) {
   products.forEach(function (product) {
     productsContainer.innerHTML += `
             <div class="product col-sm-12 col-md-6 col-lg-3">
+            <a href="./details.html?id=${product.id}" class="product__content">
                 <img src="${product.image_url}" alt="${product.title}" class="product__image">
-                <div class="product__content">
                     <h5 class="product__title">${product.title}</h5>      
-                   
                     <p class="product__price">${product.price} NOK</p>
-                </div>
                 <a href="./details.html?id=${product.id}" class="btn btn--primary btn--sm product__button">
                     details
+                </a>
                 </a>
             </div>
 
